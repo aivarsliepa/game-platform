15118dcdafeb12c6b4673ed068da92afe62f979b
Aivars Liepa <aiciz@inbox.lv>
2018-01-21 16:45:38 +0200

    added heroku config

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,14 @@ import * as socketIO from "socket.io";
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send({ msg: "Hello There!" });
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const server = http.createServer(app);
 export const io = socketIO(server);
